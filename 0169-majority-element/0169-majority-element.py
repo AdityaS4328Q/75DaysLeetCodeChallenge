3a433e4479a07142ac9317bd765615b4244348b3
AdityaS4328Q <adityas4328q@gmail.com>
2026-07-06 11:02:14 +0530

    Time: 4 ms (70.7%), Space: 21.3 MB (19.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,6 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        res = 0
-        count = 0
-        
-        for n in nums:
-            # If count is 0, we pick the current number as our new candidate
-            if count == 0:
-                res = n
-            
-            # If current number matches candidate, increment count
-            # Otherwise, decrement count
-            if n == res:
-                count += 1
-            else:
-                count -= 1
-                
-        return res
+        c = Counter(nums)
+        max_key= max(c, key= c.get)
+        return max_key
