741b8fe03241ec1db3702a476fdca3c0b11971dc
AdityaS4328Q <adityas4328q@gmail.com>
2026-07-07 12:08:07 +0530

    Time: 47 ms (71.43%), Space: 36.6 MB (66.55%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,27 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # Map to store the length of the sequence a number belongs to
-        # key: number, value: length of the sequence it's part of
-        res = {}
-        max_len = 0
+        s = set(nums)
+        longest = 0
+
+        for num in s:
+            if num - 1 not in s:
+                next_num = num + 1
+                length = 1
+                while next_num in s:
+                    length += 1
+                    next_num += 1
+                longest = max(longest, length)
         
-        for n in nums:
-            # Skip duplicates to avoid double-counting
-            if n not in res:
-                # Find the lengths of sequences to the left and right
-                left = res.get(n - 1, 0)
-                right = res.get(n + 1, 0)
-                
-                # The total length of the sequence n joins
-                current_len = left + right + 1
-                max_len = max(max_len, current_len)
-                
-                # Mark n as visited (value doesn't matter much here, but we set it)
-                res[n] = current_len
-                
-                # Crucial step: Update the "outer limits" of the sequence
-                # The boundary nodes now represent the new total length
-                res[n - left] = current_len
-                res[n + right] = current_len
-                
-        return max_len
+        return longest
