38f8bee8dc7e7be572194f320285891412339ce4
AdityaS4328Q <adityas4328q@gmail.com>
2026-08-01 09:12:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.54%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,13 +6,14 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # 1. Both nodes are null: they are identical
-        if not p and not q:
-            return True
-        
-        # 2. One node is null while the other isn't, OR values don't match
-        if not p or not q or p.val != q.val:
-            return False
-        
-        # 3. Recursively check left and right subtrees
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        def isbalanced(p,q):
+            if not p and not q:
+                return True
+            if (p and not q) or (not p and q):
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return isbalanced(p.left, q.left) and isbalanced(p.right, q.right)
+        return isbalanced(p,q)
