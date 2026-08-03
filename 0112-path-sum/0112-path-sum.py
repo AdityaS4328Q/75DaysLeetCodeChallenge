06023d11299536aa5e2ae7f7728913b00a8d1945
AdityaS4328Q <adityas4328q@gmail.com>
2026-08-03 11:54:48 +0530

    Time: 0 ms (100%), Space: 20.2 MB (56.41%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,17 +6,12 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # 1. Base case: If the node is null, no path exists
-        if not root:
-            return False
+        def has_sum(root, curr_sum):
+            if not root:
+                return False
+            curr_sum+=root.val
+            if not root.left and not root.right:
+                return curr_sum==targetSum
+            return has_sum(root.left,curr_sum) or has_sum(root.right, curr_sum)
+        return has_sum(root, 0)
         
-        # 2. Check if we are at a leaf node
-        if not root.left and not root.right:
-            return targetSum == root.val
-        
-        # 3. Update the targetSum for the children
-        new_sum = targetSum - root.val
-        
-        # 4. Recursively check left or right subtrees
-        return self.hasPathSum(root.left, new_sum) or \
-               self.hasPathSum(root.right, new_sum)
