1e3e0e4b9d249bc7c258b2e9ae8161445a811e92
AdityaS4328Q <adityas4328q@gmail.com>
2026-08-06 08:37:28 +0530

    Time: 0 ms (100%), Space: 19.9 MB (61.16%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -4,36 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import collections
-
+from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        if not root:
-            return res
+        ans=[]
+        if root is None:
+            return ans
         
-        # Initialize a queue and add the root node
-        q = collections.deque()
-        q.append(root)
+        queue = deque()
         
-        while q:
-            # How many nodes are currently in the queue?
-            # This tells us exactly how many nodes are on the current level.
-            level_length = len(q)
-            level_vals = []
-            
-            # Process all nodes on the current level
-            for i in range(level_length):
-                node = q.popleft()
-                level_vals.append(node.val)
-                
-                # Add children to the queue for the NEXT level
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            
-            # Add the current level's values to our final result
-            res.append(level_vals)
-            
-        return res
+        queue.append(root)
+        while queue:
+            level = []
+            n =  len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            ans.append(level)
+        return ans
